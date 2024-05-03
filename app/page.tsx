ded32fad2fff6ff6fa8a16ba578c4c36a10b60c6
hi-lee-mon <shunmel.09@gmail.com>
2024-05-03 18:32:35 +0900

    feat:バトル機能の追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,197 @@
-import Image from "next/image";
+'use client'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import { useMemo, useState } from 'react'
+import { Howl } from 'howler'
 
-export default function Home() {
+const Vitality = ({
+  hp,
+  name,
+  level,
+}: {
+  hp: number
+  name: string
+  level: string
+}) => {
+  const denominator = useMemo(() => hp, [])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="w-1/4 min-w-80">
+      <div className="border p-6 rounded-3xl shadow">
+        <div className="flex justify-between">
+          <div className="flex">
+            <h2>{name}</h2>
+            <p>♂</p>
+          </div>
+          <p>{`Lv.${level}`}</p>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <p>HP:</p>
+          <div className="h-3 border rounded-full overflow-hidden w-full">
+            <div
+              className={cn(
+                'size-full transition duration-1000 origin-left',
+                hp > 50
+                  ? 'bg-green-500'
+                  : hp > 20
+                    ? 'bg-yellow-500'
+                    : 'bg-red-500',
+              )}
+              style={{ transform: `scaleX(${hp / denominator})` }}
+            ></div>
+          </div>
         </div>
       </div>
+    </div>
+  )
+}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+const sound = new Howl({
+  src: ['/smallKick.mp3'],
+  html5: true,
+})
+sound.volume(0.2)
+
+export default function Home() {
+  const [hp, setHp] = useState(200)
+  const [enemyHp, setEnemyHp] = useState(100)
+  const [isOpenSkillCard, setIsOpenSkillCard] = useState(false)
+  const [message, setMessage] = useState('どうする？')
+
+  const attack = (attackPoint: number, target: 'player' | 'enemy') => {
+    if (target === 'enemy') {
+      setEnemyHp((oldHp) => Math.max(oldHp - attackPoint, 0))
+    }
+    if (target === 'player') {
+      setHp((oldHp) => Math.max(oldHp - attackPoint, 0))
+    }
+  }
+
+  const win = enemyHp === 0
+
+  const skills = [
+    {
+      name: '10万ボルト',
+      attackPoint: 60,
+    },
+    {
+      name: 'でんこうせっか',
+      attackPoint: 30,
+    },
+    {
+      name: 'アイアンテール',
+      attackPoint: 40,
+    },
+    {
+      name: 'ボルテッカー',
+      attackPoint: 100,
+    },
+  ]
+
+  return (
+    <div className="container py-20 px-80">
+      <div className="flex justify-between items-center mb-20">
+        <Vitality hp={enemyHp} name="ピカチュウ" level="50" />
+        <div className="flex flex-col justify-center">
+          <Image
+            className={cn(
+              'ml-16 transition-all duration-500 -mb-5 z-10',
+              win ? 'translate-y-10 opacity-0 delay-500' : '',
+            )}
+            src="/neko.png"
+            width={160}
+            height={80}
+            alt="野生のピカチュウ"
+          />
+          <div
+            className={cn(
+              'border h-10 ml-10 rounded-full',
+              win ? 'translate-y-10 opacity-0 delay-500' : '',
+            )}
+          />
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex justify-between items-end mb-5">
+        <div className="flex flex-col justify-center">
+          <Image
+            className="ml-8 -mb-5 z-10"
+            src="/neko.png"
+            width={160}
+            height={80}
+            alt="自分のピカチュウ"
+          />
+          <div className="border h-10 rounded-full" />
+        </div>
+        <Vitality hp={hp} name="ピカチュウ" level="60" />
       </div>
-    </main>
-  );
+      <div className="relative flex justify-between gap-3">
+        <div className="border rounded-3xl p-3 w-full shadow whitespace-pre-line">
+          {win ? 'やせいの　ピカチュウは　たおれた！' : message}
+        </div>
+        {isOpenSkillCard && (
+          <div className="absolute left-2 flex flex-col justify-between border rounded-3xl p-6 h-60 w-48 -mt-32 bg-white z-20">
+            {skills.map((skill) => (
+              <Button
+                key={skill.name}
+                variant="ghost"
+                onClick={() => {
+                  attack(skill.attackPoint, 'player')
+                  sound.play()
+                  setMessage(`ピカチュウの\n${skill.name}!`)
+                  attack(
+                    skills[Math.floor(Math.random() * 3)].attackPoint,
+                    'enemy',
+                  )
+                  setIsOpenSkillCard(false)
+                }}
+              >
+                {skill.name}
+              </Button>
+            ))}
+          </div>
+        )}
+
+        <div className="border rounded-3xl p-3 shadow grid grid-cols-2 gap-2">
+          <Button
+            variant="ghost"
+            onClick={() => {
+              setMessage('')
+              setIsOpenSkillCard(true)
+            }}
+          >
+            たたかう
+          </Button>
+          <Button
+            variant="ghost"
+            onClick={() => {
+              setIsOpenSkillCard(false)
+              setMessage('バッグが空だ！')
+            }}
+          >
+            バッグ
+          </Button>
+          <Button
+            variant="ghost"
+            onClick={() => {
+              setIsOpenSkillCard(false)
+              setMessage('手持ちのポケモンがいない')
+            }}
+          >
+            ポケモン
+          </Button>
+          <Button
+            variant="ghost"
+            onClick={() => {
+              setIsOpenSkillCard(false)
+              setMessage('今は逃げられない')
+            }}
+          >
+            逃げる
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
 }
